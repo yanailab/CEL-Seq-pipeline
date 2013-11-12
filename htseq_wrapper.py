735894a27455d2a74bf2758e23d7a6aec0e0110b
Yaron de Leeuw <me@jarondl.net>
2013-11-12 13:30:05 +0200

    htseq now ignores empty files

--- htseq_wrapper.py
+++ htseq_wrapper.py
@@ -9,13 +9,14 @@ import subprocess
 from logging import getLogger
 import shlex
 import os.path
+import csv
 
 logger = getLogger("pijp.htseq")
 
 def main(input_files, gff_file, output_dir, extra_params, count_filename):
 
     htout = []
-    base_names = ['sample']
+    base_names = []
     for sam_file in input_files:
 
         #  we need base_names for heading the matrix file.
@@ -23,14 +24,20 @@ def main(input_files, gff_file, output_dir, extra_params, count_filename):
 
         htseq_cmd =  ["htseq-count"] + shlex.split(extra_params) + [ sam_file, gff_file]
         logger.info("ran  : " + " ".join( htseq_cmd))
-        htout += [subprocess.check_output(htseq_cmd).splitlines()]
+        try:
+            htout += [subprocess.check_output(htseq_cmd).splitlines()]
+        except subprocess.CalledProcessError:
+            if os.stat(sam_file).st_size == 0:
+                logger.error(" Empty sam file : %s ", sam_file)
+            else:
+                raise
     
-    matrix = ['\t'.join(base_names) + "\n"]
+    matrix = [["#Sample:"]+base_names]
     for pairs in zip(*htout):
         first_col = pairs[0].split('\t')[0]  # first half of first pair
-        rest = (pair.split('\t')[1] for pair in pairs)
-        matrix += [first_col +'\t' + '\t'.join(rest)+"\n"] 
+        second_halves = [pair.split('\t')[1] for pair in pairs]
+        matrix += [[first_col] + second_halves] 
     with open(os.path.join(output_dir, count_filename), "w") as fh:
-       
-        fh.writelines(matrix)
+        matwriter = csv.writer(fh, delimiter='\t')
+        matwriter.writerows(matrix)
         
