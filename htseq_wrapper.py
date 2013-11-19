9b0745e64b25415344e7a96e0074be10236fe6e2
Yaron de Leeuw <me@jarondl.net>
2013-11-19 11:07:35 +0200

    switched htseq to pool, to fix number of processes

--- htseq_wrapper.py
+++ htseq_wrapper.py
@@ -12,41 +12,37 @@ import os.path
 import csv
 from itertools import cycle
 
+from multiprocessing.pool import ThreadPool
+
 logger = getLogger("pijp.htseq")
 
-def main(input_files, gff_file, output_dir, extra_params, count_filename):
+def run_cmd(cmd):
+    logger.info("ran  : " + " ".join( cmd))
+    return subprocess.check_output(cmd).splitlines()
+
+def main(input_files, gff_file, output_dir, extra_params, count_filename, procs=50):
 
     # The first col we need only once, as it is always the same.
     # So we put None, and try to write it on our first chance.
     ht_col1 = None
     ht_col2 = []
     base_names = []
-    processes = []
+    cmds = []
     for sam_file in input_files:
 
+       base_names += [os.path.splitext(os.path.basename(sam_file))[0]] 
         #  we need base_names for heading the matrix file.
-        base_names += [os.path.splitext(os.path.basename(sam_file))[0]] 
-
-        htseq_cmd =  ["htseq-count"] + shlex.split(extra_params) + [ sam_file, gff_file]
-        logger.info("ran  : " + " ".join( htseq_cmd))
-        processes.append( subprocess.Popen((htseq_cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE))
-
-    for pro in processes:
-        (stdout, stderr) = pro.communicate()
-        if pro.returncode == 0:
-            htout = (x.split('\t') for x in  stdout.splitlines())
+       htseq_cmd =  ["htseq-count"] + shlex.split(extra_params) + [ sam_file, gff_file]
+       cmds.append(htseq_cmd)
+    
+    pool = ThreadPool(procs)
+    results = pool.map(run_cmd, cmds)
+    for res in results:
+            htout = (x.split('\t') for x in  res)
             htout1, htout2 = zip(*htout)
             if ht_col1 is None:
                 ht_col1 = htout1
             ht_col2.append(htout2)
-        else:
-            if os.stat(sam_file).st_size == 0:
-                logger.error(" Empty sam file : %s ", sam_file)
-                # for an empty sam file, we want a lot of zeros..
-                ht_col2.append(cycle("0"))
-            else:
-                logger.error(" HTSeq error %d : %s", pro.returncode, stderr)
-                raise Exception("There was some error with HTSeq") 
     
     matrix_header = ["#Sample:"] + base_names
 
