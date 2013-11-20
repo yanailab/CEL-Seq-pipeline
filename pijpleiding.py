7eec3463b556bf55031c7aac7b67c4723c40a2f3
Yaron de Leeuw <me@jarondl.net>
2013-11-20 12:11:06 +0200

    added some documentation to pijpleiding, also refractored create_dir

--- pijpleiding.py
+++ pijpleiding.py
@@ -30,9 +30,15 @@ import json
 
 import bowtie_wrapper, bc_demultiplex, htseq_wrapper, clean_up
 
-
+###################################################################################################
+## sections are the names of sections in the config file.
+## segments are the functions you run. The segments and sections MUST be ordered
+## the same way.
 SECTIONS = ( "bc_demultiplex", "bowtie_wrapper", "htseq_wrapper", "clean_up")
 SEGMENTS = ( bc_demultiplex.main, bowtie_wrapper.main, htseq_wrapper.main, clean_up.main)
+###################################################################################################
+
+# some definitions for the loggers.
 
 LOGFORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 logger = logging.getLogger('pijp')
@@ -50,47 +56,63 @@ def main(config_file):
 
         if config.getboolean(section, "pipe_run"):
             parameters = dict(config.items(section))
-            parameters.pop("pipe_run")
+            parameters.pop("pipe_run")  #  Remove this from the dictionary, so it will not be passed to the segment.
+
+            ##  Read the input file list.
+            ##  pipe_input_files can have multiple lines, and each line is a glob pattern,
+            ##  i.e. include "*" and "?" as in regular bash.
             parameters["input_files"] = []
             for input_glob_pattern in parameters.pop("pipe_input_files").splitlines():
                 gl = glob(input_glob_pattern)
                 assert (len(gl) != 0 ), "input files not found for pattern "+input_glob_pattern
                 parameters["input_files"] += gl
-
             parameters["input_files"].sort()
 
-            # create the output directory if nonexistant
-            try:
-                os.makedirs(parameters['output_dir'])
-            except OSError:
-                # directory already exists..
-                if len(os.listdir(parameters['output_dir'])) != 0:
-                    ans = None
-                    while ans not in ["y", "n"]:
-                        ans = raw_input("Writing to a non-empty directory. Files may be overwritten. Are you sure? [y/n] : ")
-                    if ans == "n":
-                        exit(1)
+            ##
+            create_dir(parameters['output_dir'])
 
             # add a log file in the output dir
             log_fname = os.path.join(parameters['output_dir'], "pijp.log")
             hdlr = logging.FileHandler(log_fname)
             hdlr.setFormatter(log_formatter)
             logger.addHandler(hdlr)
-            
             logger.info("========== writing log to file at %s ==========", log_fname)
             logger.info("Section : %s",section)
             logger.info("parameters : %s", json.dumps(parameters))
-            # run the command
-            segment(**parameters)
 
-            logger.info("===== closing log")
+
+            ####  run the command ============================================
+            # that's the heart of the whole pipeline
+            segment(**parameters)
+            #### =============================================================
+
             # remove the log file handler:
+            logger.info("===== closing log")
             logger.removeHandler(hdlr)
 
 
     logger.info("===== Successfuly finished pijpleiding")
 
+
+
+
+def create_dir(dirname):
+    ## create the output directory if nonexistant
+    try:
+        os.makedirs(dirname)
+    except OSError:
+        # directory already exists..
+        if len(os.listdir(dirname)) != 0:
+            ans = None
+            while ans not in ["y", "n"]:
+                ans = raw_input("Writing to a non-empty directory. Files may be overwritten. Are you sure? [y/n] : ")
+            if ans == "n":
+                exit(1)
+
+
 if __name__ == "__main__":
+
+    ##  Parse command line options. This adds the useful --help option.
     parser = argparse.ArgumentParser(description= __doc__, formatter_class=argparse.RawDescriptionHelpFormatter,)
     parser.add_argument('config_file', type=str)
     args = parser.parse_args()
