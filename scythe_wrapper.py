99fe6fe6db0a8954fb92844f6bddee50b65a7efd
Yaron de Leeuw <me@jarondl.net>
2014-01-05 19:35:32 +0200

    scythe gz naming resolved

--- scythe_wrapper.py
+++ scythe_wrapper.py
@@ -16,6 +16,9 @@ def main(input_files, output_dir, adaptor_file, extra_params=""):
     for fastq_file in input_files:
         base_fastq = os.path.basename(fastq_file)
         output_file =  os.path.join(output_dir, base_fastq)
+        # sometimes base_fastq contains gz, while the output is plaintext
+        if output_file.endswith('.gz'):
+            output_file = output_file[:-3]
 
         scythe_cmd = "scythe -q sanger {3} -a {0} {1} -o {2}".format(adaptor_file, fastq_file, output_file, extra_params)
         logger.info("ran  :" + scythe_cmd)
