5c59efce9eb8343ba28725133aed9289d47da059
Yaron de Leeuw <me@jarondl.net>
2014-02-27 16:37:43 +0200

    parallel mapping & fixed stats

--- bowtie_wrapper.py
+++ bowtie_wrapper.py
@@ -9,12 +9,11 @@ import shlex
 import argparse
 import csv
 from glob import glob
+from multiprocessing.pool import ThreadPool
 
 logger = getLogger('pijp.bowtie_wrapper')
 
 def build_bowtie_command(fastq_file,  index_file, number_of_threads, output_dir, extra_params):
-
-
     base_fastq = os.path.splitext(os.path.basename(fastq_file))[0]
     samfile = os.path.join(output_dir, base_fastq + ".sam")
 
@@ -23,27 +22,39 @@ def build_bowtie_command(fastq_file,  index_file, number_of_threads, output_dir,
     bowtie_cmd = "bowtie2  -p {0} {1} {2} -U {3} -S {4} ".format(number_of_threads, extra_params, index_file, fastq_file, samfile)
     return bowtie_cmd
 
+def run_cmd((cmd,fastq_file)):
+    """  Run the command, and return the stderr. 
+    """
+    logger.info("ran  : " + cmd)
+    pro = subprocess.Popen(cmd, shell=True, stderr = subprocess.PIPE)
+    (x, stderr) = pro.communicate()
+    assert (pro.returncode == 0 ), "bowtie error %d : %s" % (pro.returncode, stderr)
+    new_row = ( [fastq_file] + get_stats(stderr.splitlines()) )
+    logger.info("finished  : " + cmd)
+    return new_row
 
-def main(input_files, index_file, number_of_threads, output_dir, bowtie_report_name,extra_params):
-
+def main(input_files, index_file, number_of_threads, output_dir, bowtie_report_name,extra_params,procs=10):
     report = []
-
+    ht_col2 = []
+    base_names = []
+    cmds = []
+    for fastq_file in input_files:
+       base_names += [os.path.splitext(os.path.basename(fastq_file))[0]]
+       #  we need base_names for heading the matrix file.
+       bt2_cmd = build_bowtie_command(fastq_file, index_file, number_of_threads, output_dir, extra_params)       
+       cmds.append((bt2_cmd,fastq_file))
+    ht_col1 = "\n".join(base_names)
+    pool = ThreadPool(int(procs))
+    results = pool.map(run_cmd, cmds)
+    for res in results:
+        htout = "\t".join(res)
+        ht_col2.append(htout)
+    matrix_header = "\t".join(['#sample','total', 'not_aligned', 'aligned_once', 'multi_aligned', '% mapped']) + "\n"
+    matrix = "\n".join(ht_col2)
     filename = os.path.join(output_dir, bowtie_report_name)
     with open(filename, 'wb') as f:
-        writer = csv.writer(f, delimiter='\t')
-        writer.writerow(('#sample','total', 'not_aligned', 'aligned_once', 'multi_aligned'))
-        for fastq_file in input_files:
-            bt2_cmd = build_bowtie_command(fastq_file, index_file, number_of_threads, output_dir, extra_params)
-        
-            logger.info("ran  :" + bt2_cmd)
-            #subprocess.check_call(bt2_cmd, shell=True)
-            pro = subprocess.Popen(bt2_cmd, shell=True, stderr = subprocess.PIPE)
-            (x, stderr) = pro.communicate()
-            assert (pro.returncode == 0 ), "bowtie error %d : %s" % (pro.returncode, stderr)
-    
-            new_row = ( [fastq_file] + get_stats(stderr.splitlines()) )
-         
-            writer.writerow(new_row)
+        f.write(matrix_header)
+        f.write(matrix)
 
 def get_stats(bt_stderr):
     ## Write all warnings to log, and skip them.
@@ -55,10 +66,11 @@ def get_stats(bt_stderr):
         logger.info("Bowtie probably had {0} warnings [e.g. read too small]".format(len(bt_stderr)-4))
         bt_stderr = bt_stderr[-5:]
     if int(bt_stderr[0].split()[0]) != 0 :
-        total = bt_stderr[1].split()[0]
-        not_aligned = bt_stderr[2].split()[0]
-        aligned = bt_stderr[3].split()[0]
-        multi_aligned =bt_stderr[4].split()[0] 
-        return [total, not_aligned, aligned, multi_aligned]
+        total = str(int(bt_stderr[0].split()[0]))
+        not_aligned = str(int(bt_stderr[1].split()[0]))
+        aligned = str(int(bt_stderr[2].split()[0]))
+        multi_aligned = str(int(bt_stderr[3].split()[0]))
+        mapped_percent = bt_stderr[4].split()[0]
+        return [total, not_aligned, aligned, multi_aligned, mapped_percent]
     else:
-        return [0,0,0,0]
+        return [0,0,0,0,0]
